74762e41fb636c269530a125ec92d0d08703a872
Gentle <muhammetdoger@hotmail.com>
2022-11-30 20:44:03 +0300

    CSS / styled styled & Bootstrap & Tailwind

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import styles from './App.module.css';
+import Test from './Test';
+import { Title } from './Components';
+import Bootstrap from './Bootstrap';
+import Tailwind from './Tailwind';
+import './tailwind.css';
 
 function App() {
 	return (
-		<div className='App'>
-			<h3>{process.env.NODE_ENV}</h3>
+		<div className={styles.App}>
+			<Title>{process.env.NODE_ENV}</Title>
+			<Title theme='dark'>{process.env.NODE_ENV}</Title>
 			<p>{process.env.REACT_APP_API_URL}: Test</p>
+			<Test />
 			{process.env.NODE_ENV === 'production' && (
 				<>
 					<img src='/logo192.png' alt='' />
 				</>
 			)}
+			<Bootstrap />
+			<Tailwind />
 		</div>
 	);
 }
---
+++ src/Test.js
@@ -0,0 +1,5 @@
+function Test() {
+	return <div>Test component</div>;
+}
+
+export default Test;

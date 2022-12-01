c698100fd6939e28bc5f776972343956c0ebb08e
Gentle <muhammetdoger@hotmail.com>
2022-12-01 11:01:44 +0300

    css full + scss (sass)

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Test from './Test';
 import { Title } from './Components';
 import Bootstrap from './Bootstrap';
 import Tailwind from './Tailwind';
-import './tailwind.css';
+import './style.scss';
+// import './tailwind.css';
 
 function App() {
 	return (
 		<div className={styles.App}>
 			<Title>{process.env.NODE_ENV}</Title>
+			<p className='env'>
+				<span>Test</span>
+			</p>
 			<Title theme='dark'>{process.env.NODE_ENV}</Title>
 			<p>{process.env.REACT_APP_API_URL}: Test</p>
 			<Test />
